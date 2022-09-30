c24782ea055e292a1df8fbc3e3bdc068398e43dc
WooHCode <gusdn526@naver.com>
2022-09-30 23:07:43 +0900

    Second commit // 1. spring jdbc template 2. JPA 3. Spring data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,43 @@
 package hello.hellospring;
 
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.lang.invoke.StringConcatException;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    private final DataSource dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository){
+        this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+
+ //   @Bean
+   // public MemberRepository memberRepository(){
+        // return new MemoryMemberRepository();
+       // return new JdbcMemberRepository(dataSource);
+       // return new JdbcTemplateMemberRepository(dataSource);
+     //   return new JpaMemberRepository(em);
+
+    //}
 }
